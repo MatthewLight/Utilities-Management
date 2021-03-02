331cd6f472f6122df13e1b8e5f471260b651e67a
Andrii Blyshchak <andrewblyshchak@gmail.com>
2021-03-02 23:41:46 +0200

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ msg: '200' });
+});
+
+const { PORT } = process.env;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
